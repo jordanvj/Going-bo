6a89b82382411805086e267741190fcef47a002c
jodan_vj <jadonwinay@gmail.com>
2018-02-01 22:29:59 +0530

    My Project Setup

---
+++ goingbackend/src/main/java/net/van/goingbackend/App.java
@@ -0,0 +1,13 @@
+package net.van.goingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
